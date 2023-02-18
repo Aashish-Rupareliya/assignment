52eb3aa40eabaa81f937f18450abcb60da28dd70
Aashish <124659637+Aashish-Rupareliya@users.noreply.github.com>
2023-02-18 15:34:27 +0530

    Add files via upload

---
+++ MODULE 3.1/NAME/NAME/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  NAME
+//
+//  Created by apple on 18/02/23.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("MY NAME : AASHISH");
+    printf("\nMY BIRTH DATE : 02/06/1997");
+    printf("\nMY AGE : 25");
+    printf("\nADDRESS : AHMEDABAD");
+}
